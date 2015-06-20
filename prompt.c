01dba01f8046f2aa265a326825a991b66175eb86
Nicolas Arkkila <nicolas@arkkila.fi>
2015-06-21 02:11:58 +0300

    Added specific ifdefs for osx/win/linux

--- prompt.c
+++ prompt.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+char* readline(char* prompt) {
+	fputs(prompt, stdout);
+	fgets(buffer, 2048, stdin);
+	char* copy = malloc(strlen(buffer)+1);
+	strcpy(copy, buffer)
+	copy[strlen(copy)-1] = '\0';
+	return cpy;
+}
+
+char* add_history(char* _) {}
+#endif
+
+#ifdef __APPLE__
 #include <editline/readline.h>
+#endif
+
+#ifdef __linux
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
+
 
 int main(int argc, char** argv) {
 	puts("wewlisp version 0.0.1");
