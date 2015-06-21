3c435d27df095377db304a81b2750e99b2e57afc
Nicolas Arkkila <nicolas@arkkila.fi>
2015-06-21 13:45:10 +0300

    Include mpc header, added simple mpc utilising grammar and evaluation for prompt

--- prompt.c
+++ prompt.c
@@ -1,18 +1,19 @@
+#include "mpc.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #ifdef _WIN32
 #include <string.h>
-
 static char buffer[2048];
 
 char* readline(char* prompt) {
-	fputs(prompt, stdout);
-	fgets(buffer, 2048, stdin);
-	char* copy = malloc(strlen(buffer)+1);
-	strcpy(copy, buffer)
-	copy[strlen(copy)-1] = '\0';
-	return cpy;
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* copy = malloc(strlen(buffer)+1);
+    strcpy(copy, buffer)
+    copy[strlen(copy)-1] = '\0';
+    return cpy;
 }
 
 char* add_history(char* _) {}
@@ -29,18 +30,45 @@ char* add_history(char* _) {}
 
 
 int main(int argc, char** argv) {
-	puts("wewlisp version 0.0.1");
-	puts("Press Ctrl+c to exit");
 
-	while(1) {
-		char* input = readline("wewlisp> ");
+  /* Create Some Parsers */
+  mpc_parser_t* Number   = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr     = mpc_new("expr");
+  mpc_parser_t* Wewlisp    = mpc_new("wewlisp");
 
-		add_history(input);
+  /* Define them with the following Language */
+  mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                     \
+      number   : /-?[0-9]+/ ;                             \
+      operator : '+' | '-' | '*' | '/' ;                  \
+      expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+      wewlisp  : /^/ <operator> <expr>+ /$/ ;             \
+    ",
+    Number, Operator, Expr, Wewlisp);
 
-		printf("No you're a %s\n", input);
+    puts("wewlisp version 0.0.2");
+    puts("Press Ctrl+c to exit");
 
-		free(input);
-	}
+    while(1) {
+        char* input = readline("wewlisp> ");
 
-	return 0;
+        add_history(input);
+
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, Wewlisp, &r)) {
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        }
+        else {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
+
+        free(input);
+    }
+
+    mpc_cleanup(4, Number, Operator, Expr, Wewlisp);
+
+    return 0;
 }
